28aaa7dda1ce7266b3eea02a8b11f32529eedaaa
chrontax <chrontax@proton.me>
2025-09-06 17:00:45 +0200

    fix: check if booru url is valid

--- app/src/main/java/org/chrontax/booru_viewer/data/preferences/DefaultPreferencesRepository.kt
+++ app/src/main/java/org/chrontax/booru_viewer/data/preferences/DefaultPreferencesRepository.kt
@@ -4,6 +4,8 @@ import androidx.datastore.core.DataStore
 import kotlinx.coroutines.flow.Flow
 import org.chrontax.booru_viewer.data.preferences.proto.BooruSite
 import org.chrontax.booru_viewer.data.preferences.proto.Preferences
+import java.net.MalformedURLException
+import java.net.URL
 import javax.inject.Inject
 
 class DefaultPreferencesRepository @Inject constructor(private val preferencesDataStore: DataStore<Preferences>) :
@@ -11,6 +13,8 @@ class DefaultPreferencesRepository @Inject constructor(private val preferencesDa
     override val preferencesFlow: Flow<Preferences> = preferencesDataStore.data
 
     override suspend fun addBooruSite(booruSite: BooruSite) {
+        checkUrl(booruSite.url)
+
         preferencesDataStore.updateData { currentPreferences ->
             if (currentPreferences.sitesList.any { it.id == booruSite.id }) {
                 throw IllegalArgumentException("A site with ID '${booruSite.id}' already exists.")
@@ -30,6 +34,8 @@ class DefaultPreferencesRepository @Inject constructor(private val preferencesDa
     }
 
     override suspend fun updateBooruSite(booruSite: BooruSite) {
+        checkUrl(booruSite.url)
+
         preferencesDataStore.updateData { currentPreferences ->
             val siteIndex = currentPreferences.sitesList.indexOfFirst { it.id == booruSite.id }
             if (siteIndex == -1) {
@@ -38,5 +44,16 @@ class DefaultPreferencesRepository @Inject constructor(private val preferencesDa
             currentPreferences.toBuilder()
                 .setSites(siteIndex, booruSite).build()
         }
+    }
+}
+
+fun checkUrl(url: String) {
+    try {
+        val url = URL(url)
+        if (url.protocol != "http" && url.protocol != "https") {
+            throw IllegalArgumentException("Invalid URL protocol: ${url.protocol}")
+        }
+    } catch (e: MalformedURLException) {
+        throw IllegalArgumentException("Invalid URL: $url")
     }
 }
