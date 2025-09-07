1a77c6a96442829d6702caf99c0be27e60303616
chrontax <chrontax@proton.me>
2025-09-07 14:44:40 +0200

    chore: lower minSdk to 21

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
 
     defaultConfig {
         applicationId = "org.chrontax.booru_viewer"
-        minSdk = 24
+        minSdk = 21
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
